aebc91e1e9c133f9a240d3ea5cf239e8869e5f3e
Bainash <nischalbaidar@yahoo.com>
2024-05-08 18:32:54 +0545

    Changed

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-# def generate_certificates(names, template_path, output_folder):
-#     """Generate certificates for a list of names."""
-#     # Iterate over each name and generate a certificate for it
-#     for index, name in enumerate(names):
-#         generate_certificate(name, template_path, output_folder)
-#         print(f"Processing {index + 1} / {len(names)}")
